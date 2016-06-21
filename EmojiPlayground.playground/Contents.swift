5777ef8d0e44be91f5bada3674c532b0837932bc
Jack Morris <jxmorris12@gmail.com>
2016-06-21 11:19:31 -0400

    added Swift playgrounds file

---
+++ EmojiPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+typealias 🌭🌭🌭🌭🌭🌭 = String
+typealias 🎾 = UnicodeScalar
+
+let 🎲 = 0x1F601
+let 🎸 = 0x1F64F
+
+let 🌵 = " "
+var 🏈 = ""
+
+for 🐶 in 🎲...🎸 {
+  var 👽 = 🌭🌭🌭🌭🌭🌭(🎾(🐶))
+  🏈 += 👽 + 🌵
+}
+
+print(🏈)
